7c5321897a8e52e55bf8cda1764147705e648b64
bhautik patel <bdudhat293@gmail.com>
2023-12-18 09:34:56 +0530

    18-12-2023

--- q1.js
+++ q1.js
@@ -1,41 +1,85 @@
-//  ||  REVERSE NUMBER  ||
+//  Write a JS function that reverse a number 
 
-// let arr = Number (prompt("Enter the number : "));
-
-// let r = a(arr);
-
-// console.log("Currect number is : ",arr);
-// console.log("Reverse Number is : ",r);
-
-// function a(n){
-//     let sum = 0;
-//     while (n!=0){
-//         sum = sum * 10 + n % 10;
-//         n=Math.floor(n/10);
-//     }
-//     return sum;
+// let num = Number(prompt("Enter Number : ")); 
+ 
+// let r = a(num); 
+// console.log("Currect Number : ",num);    
+// console.log("The Reverse Number is : ",r); 
+ 
+//  function a(n) { 
+//     let sum = 0 ; 
+//     while (n!=0) { 
+//         sum = sum * 10 + n%10; 
+//         n = Math.floor(n / 10); 
+//     } 
+//     return sum;  
 // }
 
-//  function arr(str){
-//     return str.split('').sort().join('');
-//  }
-//  console.log(arr("bhautik"));
+//   Write a JS function that returns a passed string with letters in alphabetical order 
 
 // function arr(str){
-//     return  str.
+//     return str  . split('').sort().join('');
 // }
-// console.log("kanubhai kacharavala surat");
 
- 
-Number = int(input(" Please Enter any Number: "));
-let i;
-Sum = 0
-for i in range(1, Number):
-    if(Number % i == 0):
-        Sum = Sum + i
-if (Sum == Number){
-    print(" %d is a Perfect Number" %Number)
-     }
-else{
-    print(" %d is not a Perfect Number" %Number)
-}
+// console.log(arr("developer"));
+
+//  Write a JS function that accepts a string as a parameter and converts the first letter of each word of the string in upper case
+
+// 3.
+
+// const text1 = 'the quick brown fox';
+// const text2 = text1.toUpperCase();
+// console.log(text1);
+
+// function capitalFirstLetter(str) {
+//     let words = str.split(' ');
+//     for (let i = 0; i < words.length; i++) {
+//       words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+//     }
+//     let capitalizedStr = words.join(' ');
+//     return capitalizedStr;
+//   }
+//   let inputString = 'the quick brown fox';
+//   let outputString = capitalFirstLetter(inputString);
+//   console.log(outputString); // Output: 'The Quick Brown Fox'
+  
+  
+
+// 4.
+  
+// function LongestWord(str) {
+//     let words = str.split(' ');
+//     let longest = '';
+  
+//     for (let i = 0; i < words.length; i++) {
+//       if (words[i].length > longest.length) {
+//         longest = words[i];
+//       }
+//     }
+  
+//     return longest;
+//   }
+  
+//   let inputString = 'Web Development Tutorial';
+//   let longest = LongestWord(inputString);
+//   console.log(longest);  // Output: 'Development'  
+
+// 5.
+function isPerfectNumber(number) {
+    let sum = 0;
+  
+    for (let i = 1; i < number; i++) {
+      if (number % i === 0) {
+        sum += i;
+      }
+    }
+  
+    if (sum === number) {
+      console.log("perfect");
+    } else {
+      console.log("not perfect");
+    }
+  }
+  let number = Number(prompt("enter value"));
+  console.log(number);
+  isPerfectNumber(number);
